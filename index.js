f101ef2a50232ec082c94502b89ff7cdbaa1a22b
daniel salgado <daniel.salgado.p@gmail.com>
2021-05-22 00:14:49 +0200

    first middleware

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+console.log('primeras'); // esto sale por la consola de express (no del navegador)
+// first middleware
+
+app.use((req, res, next)=>{
+    console.log('va por ahi');
+    next();
+});
+
 var contactRouter = require('./routes/contactRoutes')
 
 app.use('/contacts/', contactRouter);
