c0bd9d8194816fbda849798b2a66d6b41773e40c
daniel salgado <daniel.salgado.p@gmail.com>
2021-05-21 19:32:52 +0200

    routes in own file

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello world');
-})
+var contactRouter = require('./routes')
+
+app.use('/contacts/', contactRouter);
 
 app.listen(port, () => {
     console.log('listening');
