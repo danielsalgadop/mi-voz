acb261d35825533cf113ebc8127218b9eab6e6de
daniel salgado <daniel.salgado.p@gmail.com>
2021-06-05 10:11:12 +0200

    starting mysql schemas

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req, res) =>{
-    res.send('contacts')
+    res.jsonp('contacts')
 });
 
 router.get('/:id(\\d+)',(req, res) =>{
