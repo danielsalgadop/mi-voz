5b43d0e4e579457850047eb7262ebe33520cc344
daniel salgado <daniel.salgado.p@gmail.com>
2021-06-13 19:22:17 +0200

    returning json stringified from contacts

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -15,29 +15,24 @@ let connection = mysql.createConnection({
 
 connection.connect();
 
-router.get('/',(req, res) =>{
-    console.log(process.env.DB_USER);
+router.get('/', (req, res) => {
     let result = connection.query('SELECT * from contact',
-    function(err, rows, fields) {
-        let asjson = [];
-		var s="";
-		for ( var i=0; i<rows.length; i++ )
-            // asjson.push[i]=['name',rows[i].name];
-            // asjson.push[i]=['surname',rows[i].surname];
-			s+='<p>'+rows[i].name+'(created '+rows[i].surname+')</p>';
-
-		res.end( s );
-		}
+        function (err, rows, fields) {
+            let asjson = [];
+            var s = "";
+            for (var i = 0; i < rows.length; i++) {
+                asjson.push({ 'name': rows[i].name, 'surname': rows[i].surname });
+            }
+            res.end(JSON.stringify(asjson));
+        }
     );
-    // console.log(result);
-    // res.jsonp(result)
 });
 
-router.get('/:id(\\d+)',(req, res) =>{
-    res.send('contacts with id '+req.params.id)
+router.get('/:id(\\d+)', (req, res) => {
+    res.send('contacts with id ' + req.params.id)
 });
 
-router.post('/',(req, res) =>{
+router.post('/', (req, res) => {
     res.send('POSTcontacts')
 });
 module.exports = router;
