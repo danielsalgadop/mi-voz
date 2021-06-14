71447fd77d51e13ba4ab3c38f2ca5637c2a1117f
daniel salgado <daniel.salgado.p@gmail.com>
2021-06-15 00:14:02 +0200

    WIP

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
 const port = 3000;
-dotenv.config();
 
 var contactRouter = require('./routes/contactRoutes')
 
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,31 +1,31 @@
 const express = require('express');
 const router = express.Router();
-// const dotenv = require('dotenv');
-
-const mysql = require('mysql');
+const {stablishConnection} = '../connections/MysqlConnection.js';
 
 
 
-let connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-})
 
-connection.connect();
 
 router.get('/', (req, res) => {
-    let result = connection.query('SELECT * from contact',
-        function (err, rows, fields) {
-            let asjson = [];
-            var s = "";
-            for (var i = 0; i < rows.length; i++) {
-                asjson.push({ 'name': rows[i].name, 'surname': rows[i].surname });
-            }
-            res.end(JSON.stringify(asjson));
-        }
-    );
+    // res.status(200).send('toe');
+    stablishConnection()
+    // .then((connection) =>{
+    //     let result = connection.query('SELECT * from contact',
+    //     function (err, rows, fields) {
+
+    //         let asjson = [];
+    //         var s = "";
+    //         for (var i = 0; i < rows.length; i++) {
+    //             asjson.push({ 'name': rows[i].name, 'surname': rows[i].surname });
+    //         }
+    //         res.end(JSON.stringify(asjson));
+    //     }
+    //     );
+    // })
+    // .catch((error)=>{
+    //     // res.status(500).send(error);
+    //     res.status(500).send('Database Error');
+    // })
 });
 
 router.get('/:id(\\d+)', (req, res) => {
