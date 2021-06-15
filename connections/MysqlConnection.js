1d7257140e27d82cfc98b439ebe57854719a4c8c
daniel salgado <daniel.salgado.p@gmail.com>
2021-06-16 01:09:45 +0200

    require was missing so I was getting: TypeError: stablishConnection is not a function

--- connections/MysqlConnection.js
+++ connections/MysqlConnection.js
@@ -4,25 +4,41 @@ dotenv.config();
 
 
 
-module.exports.stablishConnection = ()=>{
-    return new Promise((resolve,reject)=>{
-
-        const connection = mysql.createConnection({
-            host: process.env.DB_HOST,
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_DATABASE
-        });
-        connection.connect((err) => {
-            if(err) {
-                reject(err)
-            }
-            resolve(connection)
-        });
-
-    })
+module.exports.stablishConnection = () => {
+console.log('tieasdfd')
 }
+// module.exports.stablishConnection = stablishConnection;
 
-module.exports.close = (connection)=> {
-    connection.destroy();
-}
+// let stablishConnection = () => {
+//     var connection = mysql.createConnection({
+//         host: process.env.DB_HOST,
+//         user: process.env.DB_USER,
+//         password: process.env.DB_PASSWORD,
+//         database: process.env.DB_DATABASE
+//     });
+//     connection.connect((error)=>{
+//         if(error){
+//             console.log('error')
+//         }
+//     });
+    // return new Promise((resolve,reject)=>{
+
+    //     const connection = mysql.createConnection({
+    //         host: process.env.DB_HOST,
+    //         user: process.env.DB_USER,
+    //         password: process.env.DB_PASSWORD,
+    //         database: process.env.DB_DATABASE
+    //     });
+    //     connection.connect((err) => {
+    //         if(err) {
+    //             reject(err)
+    //         }
+    //         resolve(connection)
+    //     });
+
+    // })
+// }
+
+// module.exports.close = (connection) => {
+    // connection.destroy();
+// }
--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {stablishConnection} = '../connections/MysqlConnection.js';
+const { stablishConnection } = require('../connections/MysqlConnection');
 
 
 
